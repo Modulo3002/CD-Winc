9ff006a4f8a63c00858aced771cbff1159befade
Robin <81294929+Modulo3002@users.noreply.github.com>
2024-12-17 15:13:24 +0100

    changes to ci-cd.yml and app.py -13

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "CD CD CD 111 Hello, World ! TEST TEST TEST TEST1"
+    return "HALLO!!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
